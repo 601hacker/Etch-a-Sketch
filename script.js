18d7ed9d10db783fa020c130148f3b25fbcd2e0a
601hacker <farhanishraq601@gmail.com>
2021-06-21 16:58:18 +0600

    Etch-A-Sketch complete! Added reset button and size preference

--- script.js
+++ script.js
@@ -27,3 +27,34 @@ function promptAnddivs() {
         alert('Surpassed maximum or minimum limit, press reset to try again');
     }
 };
+
+button.addEventListener('click', function() {
+    let gridSize = prompt('Pick drawing grid size~', 'Max-limit: 100');
+    let colorDivs = document.querySelectorAll('.divs');
+
+    for (let i = 0; i < colorDivs.length; i++) {
+        colorDivs[i].parentNode.removeChild(colorDivs[i]);
+    }
+
+    if (gridSize <= 100 && gridSize >= 0) {
+            for (let i = 0; i < gridSize * gridSize; i++) {
+            let divs = document.createElement('div');
+            divs.className = 'divs';
+            let divsText = document.createTextNode('');
+            divs.appendChild(divsText);
+            main.appendChild(divs);
+        }
+
+        main.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+        main.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+        let colorDivs = document.querySelectorAll('.divs');
+    
+        for (let i = 0; i < colorDivs.length; i++) {
+            colorDivs[i].addEventListener('mouseenter', function(e) {
+                e.target.style.backgroundColor = 'blue';
+            });
+        };
+    } else {
+        alert('Surpassed maximum or minimum limit, press reset to try again');
+    }
+});
